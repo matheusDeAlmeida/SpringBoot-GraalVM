20fca8a0e5bcafc6cd9ca852ccc848bc8c11adb1
Matheus Santos de Almeida <matheusalmeida@tokenlab.com.br>
2020-11-29 09:04:43 -0300

    ajustes para funcionar no docker

--- src/main/java/com/example/jpa/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpa.repository;
-import com.example.jpa.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
-    
-}
